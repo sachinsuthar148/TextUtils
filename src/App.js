069b13db9af7d17beca4f9e0ede7fbe299734ec4
2021pietcssachin148@poornima.org <2021pietcssachin148@poornima.org>
2023-08-25 20:01:40 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import About from "./components/About";
-import {BrowserRouter as Router, Route,Routes} from "react-router-dom"
+// import Sachin from "./components/Sachin";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-    
       <Router>
-    <Navbar title="TextUtils" about="About" />
-    <div className="container my-3">
-    <Routes>
-          <Route exact path="/about" element={ <About />}/>
-          <Route exact path="/" element={     <Textform  heading="Enter the text to analyze below" />}/>
-       
-      
-    </Routes>
-    </div>
-    </Router>
+        <Navbar title="TextUtils" about="About" />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route
+              exact
+              path="/"
+              element={<Textform heading="Enter the text to analyze below" />}
+            />
+          </Routes>
+        </div>
+      </Router>
     </>
-
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -1,134 +1,31 @@
-import React, { useState } from "react";
+import React from 'react'
 
-export default function About() {
+export default function about() {
 
+    const Style = {
+      fontSize: 16,
+      fontFamily: 'Times New Roman',
+      }
   
-    const [myStyle,setMyStyle] = useState
-    ({
-        color:'black',
-        backgroundColor:'white'
-    })
-
-    const [btnText,SetbtnText] = useState("Enable dark mode")
-
-    const toggleStyle = () =>
-    {
-        if(myStyle.color==='black')
-        {
-            setMyStyle({
-                color:'white',
-                backgroundColor:'black',
-                border:'1px solid white'
-            }
-            )
-            SetbtnText("Enable light mode")
-        }
-        else
-        {
-            setMyStyle({
-                color:'black',
-                backgroundColor:'white'
-            }
-            )
-            SetbtnText("Enable dark mode")
-        }
-
-    }
-
-  return (
-    <div className="container my-5" style={myStyle}>
-        <h3>About Us</h3>
-      <div class="accordion" id="accordionPanelsStayOpenExample">
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-            style={myStyle}
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseOne"
-              aria-expanded="true"
-              aria-controls="panelsStayOpen-collapseOne">
-              Accordion Item #1
-            </button>
-          </h2>
-          <div
-            id="panelsStayOpen-collapseOne"
-            className="accordion-collapse collapse show">
-            <div className="accordion-body" style={myStyle}>
-            
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-            style={myStyle}
-              class="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseTwo"
-              aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseTwo">
-              Accordion Item #2
-            </button>
-          </h2>
-          <div
-            id="panelsStayOpen-collapseTwo"
-            className="accordion-collapse collapse">
-            <div className="accordion-body" style={myStyle}>
+    return (
+      <div >
+              <h1 className='text-center'>About Us</h1>
+              <div className='container box text-center my-5'>
+                  <h2>Welcome to TextUtils – Your Ultimate Text Manipulation Hub!</h2>
+  <p className='about my-3' style={Style}>
+  Are you tired of dealing with text that's all over the place? Look no further! TextUtils is here to make your life easier. Our user-friendly website lets you take control of your text with just a few clicks. <br />
+  
+  Imagine being able to transform your text into UPPERCASE to make it stand out, or into lowercase for a more subtle touch. Got messy, cluttered text? No worries! Our "Clear Text" feature will give you a clean slate to work with. <br />
+  
+  But that's not all – ever struggled with extra spaces causing your text to look weird? We've got you covered! Use our "Remove Extra Spaces" tool to effortlessly tidy up your text and make it look polished. <br />
+  
+  Our website features a simple and intuitive interface. Just type or paste your text into the large textarea section, and then choose the manipulation you want. With TextUtils, you're in command. <br />
+  
+  Say goodbye to text troubles and hello to seamless text manipulation. Experience the power of simplicity with TextUtils today! <br />
+  </p>
+              </div>
+      
            
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header">
-            <button
-            style={myStyle}
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#panelsStayOpen-collapseThree"
-              aria-expanded="false"
-              aria-controls="panelsStayOpen-collapseThree">
-              Accordion Item #3
-            </button>
-          </h2>
-          <div
-            id="panelsStayOpen-collapseThree"
-            className="accordion-collapse collapse">
-            <div className="accordion-body" style={myStyle}>
-            
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
-            </div>
-          </div>
-        </div>
-      </div>
-      <button type="button" onClick={toggleStyle} className="btn btn-primary my-3">{btnText}</button>
-
-    </div>
-  );
+       </div>
+  )
 }
